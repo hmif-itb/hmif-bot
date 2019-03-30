71a954b0d09935f0d9d52bf490b2a346689c84ff
gilangardya <13515096@std.stei.itb.ac.id>
2019-03-30 21:38:00 +0700

    Add series to text_contains

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
-def text_contains(text, keywords):
+def text_contains(text, keywords, series=False):
+    if series:
+        idx = 0
+        for keyword in keywords:
+            new_idx = text.find(keyword)
+            if new_idx < idx:
+                return False
+            idx = new_idx
+        return True
+    
     for keyword in keywords:
         if (text.find(keyword) == -1):
             return False
