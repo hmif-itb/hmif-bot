3fa98a6804efa49dc01e83405ba7163bb7c82e4f
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-09-14 08:14:09 +0700

    add url

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ replies_massa = [
     TextSendMessage(text='Ga ada m*ss* di HMIF, adanya anggota'),
     TextSendMessage(text='M****? Tolong ini ditendang dong'),
     TextSendMessage(text='Eh siapa bilang m****? Ntar dicubit Deborah lho'),
-    ImageSendMessage(original_content_url='https://hmifbot.herokuapp.com/images/Meme-1.png',
-                     preview_image_url='https://hmifbot.herokuapp.com/images/Meme-1.png'),
-    ImageSendMessage(original_content_url='https://hmifbot.herokuapp.com/images/Meme-2.png',
-                     preview_image_url='https://hmifbot.herokuapp.com/images/Meme-2.png'),
+    ImageSendMessage(original_content_url='https://hmif-bot.herokuapp.com/images/Meme-1.png',
+                     preview_image_url='https://hmif-bot.herokuapp.com/images/Meme-1.png'),
+    ImageSendMessage(original_content_url='https://hmif-bot.herokuapp.com/images/Meme-2.png',
+                     preview_image_url='https://hmif-bot.herokuapp.com/images/Meme-2.png'),
 ]
 
 
