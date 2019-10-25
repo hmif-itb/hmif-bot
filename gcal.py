7f3937574e7888463a0f99398cfe017c4ff4b149
ManEnfu <hxrd97@gmail.com>
2019-10-25 14:18:36 +0700

    Add student year filter for assigment deadlines

--- gcal.py
+++ gcal.py
@@ -1,11 +1,21 @@
 import json
 import requests
 import sys
+from utils import text_contains
 
 endpoint = 'https://script.google.com/macros/s/AKfycbw9x9Y-9dQucsjVRoevf-QAB1kADg6Z7Sy8sS_424ueX8iRjNjU/exec'
 
 
 def getEvents(text_message, group_id, start_date=None, days=None):
+    year = 0
+
+    if text_contains(text_message, ['untuk', 'bit'], series=True):
+        year = 16
+    elif text_contains(text_message, ['untuk', 'unix'], series=True):
+        year = 17
+    elif text_contains(text_message, ['untuk', 'decrypt'], series=True):
+        year = 18
+
     param = dict()
     param['textMessage'] = text_message
     param['groupId'] = group_id
@@ -23,6 +33,12 @@ def getEvents(text_message, group_id, start_date=None, days=None):
         result = json.loads(str(r.content, 'utf-8'))
         if (result.get('code') != 'SUCCESS'):
             raise result.get('message')
+        if year > 0:
+            filtered_result = []
+            for res in result.get('result'):
+                if res['name'].find(str(year) + ']') != -1:
+                    filtered_result.append(res)
+            return filtered_result
         return result.get('result')
     else:
         raise Exception('request return with status code {}'.format(r.status_code))
